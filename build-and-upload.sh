957396a01c5f435a57986b895f905498695f7eca
Alex Williams <lexw@live.co.uk>
2019-01-27 00:06:48 +0000

    Cleaned up src build after extraction

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -5,7 +5,12 @@ set +ex
 function buildAndPush {
     local version=$1
     docker build -t alexswilliams/arm32v6-grafana:${version} --build-arg VERSION=${version} --file Dockerfile.arm32v6 .
-    docker push alexswilliams/arm32v6-grafana:${version}
+    if [[ $? -eq 0 ]]; then
+        docker push alexswilliams/arm32v6-grafana:${version}
+    else
+        echo "Failed to build; not pushing."
+        exit 1
+    fi
 }
 
 buildAndPush "5.4.3"
